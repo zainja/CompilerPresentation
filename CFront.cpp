4cd0ed31e4e47ce14933b072f1ab85bff459efac
Zain Alden Jaffal <zainjaffal@gmail.com>
2021-08-27 16:29:05 +0100

    starter for lexer

--- CFront.cpp
+++ CFront.cpp
@@ -8,6 +8,11 @@
 #include <vector>
 
 using namespace std;
+
+//===----------------------------------------------------------------------===//
+// Lexer
+//===----------------------------------------------------------------------===//
+
 // Starting Point
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -23,5 +28,42 @@ enum Token {
   tok_number = -5,
 };
 
-static std::string IdentifierStr; // Filled in if tok_identifier
+static string IdentifierStr; // Filled in if tok_identifier
 static double NumVal;             // Filled in if tok_number
+
+//TODO: Complete the lexer
+static int gettok() {
+    static int LastChar = ' ';
+    // Skip whitespace in lastChar
+    // HINT 1: USE C getChar() to get the next char in the code
+    // HINT 2: use isspace(c) to see if c is a whitespace
+
+    // recognize Strings append the characters to IdentifierStr
+
+    // recognize Numbers assign the value to NumVal
+    // Hint use strtod to get the number value
+
+
+    // disacrd comments
+    if (LastChar == '#') {
+    // Comment until end of line.
+    do
+      LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF)
+      return gettok();
+  }
+
+   // Check for end of file.  Don't eat the EOF.
+  if (LastChar == EOF)
+    return tok_eof;
+
+  // Otherwise, just return the character as its ascii value.
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
+// In case you struggle
+// https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl01.html
